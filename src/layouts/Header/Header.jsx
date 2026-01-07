d9621cbfe4958e0f5c6bf79d31ba378ce524fd2b
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-07 12:12:22 +0300

    Add header menu markup

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,11 +1,41 @@
 import Logo from "@/components"
 import "./Header.scss"
 
+const menuItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Movies & Shows",
+    href: "/movies",
+  },
+  {
+    label: "Support",
+    href: "/support",
+  },
+  {
+    label: "Subscriptions",
+    href: "/subscriptions",
+  },
+]
+
 const Header = () => {
   return (
     <header className="header">
       <div className="header__inner container">
         <Logo />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({ label, href }) => (
+              <li className="header__menu-item" key={label}>
+                <a href={href} className="header__menu-link">
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   )
