9ba0639f663e41432452a08b5ac8cf4d9d56e81b
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-17 12:29:16 +0300

    Add Footer starter markup

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,62 @@
+import "./Footer.scss"
+
+const menuItems = [
+  {
+    title: "Home",
+    links: ["Categories", "Devices", "Pricing", "FAQ"],
+  },
+  {
+    title: "Movies",
+    links: ["Gernes", "Trending", "New Release", "Popular"],
+  },
+  {
+    title: "Shows",
+    links: ["Gernes", "Trending", "New Release", "Popular"],
+  },
+  {
+    title: "Support",
+    links: ["Contact us"],
+  },
+  {
+    title: "Subscriptions",
+    links: ["Plans", "Features"],
+  },
+  {
+    title: "Connect with us",
+    socialLinks: [
+      {
+        label: "Facebook",
+        iconName: "facebook",
+      },
+      {
+        label: "Twitter",
+        iconName: "twitter",
+      },
+      {
+        label: "LinkedIn",
+        iconName: "linked-in",
+      },
+    ],
+  },
+]
+
 const Footer = () => {
-  return <footer>FOOTER</footer>
+  return (
+    <footer className="footer">
+      <div className="footer__inner containter">
+        <nav className="footer__menu">
+          {menuItems.map(({ title, links, socialLinks }) => (
+            <div className="footer__menu-column" key={title}>
+              <a href="/" className="footer__menu-title h6">
+                {title}
+              </a>
+            </div>
+          ))}
+        </nav>
+        <div className="footer__extra"> </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
