02b3f63d5bba5f35432ca727d979038ac4a96ec8
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-19 22:46:52 +0300

    Add Hero-body-inner markup

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,12 @@
-import cn from "classnames"
+import Button from "@/components/Button"
 import "./Hero.scss"
 
 const playButtonTitle = "Play Video"
+const titleId = "hero-title"
 
 const Hero = () => {
   return (
-    <section className="hero" aria-labelledby="hero-title">
+    <section className="hero" aria-labelledby={titleId}>
       <div className="hero__pano">
         <div className="hero__pano-inner container">
           <button
@@ -25,7 +26,27 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-body">
-        <div className="hero-body-inner container"></div>
+        <div className="hero-body-inner container">
+          <h1 className="hero__title" id={titleId}>
+            The Best Streaming Experience
+          </h1>
+          <div className="hero__description">
+            <p>
+              StreamVibe is the best streaming experience for watching your
+              favorite movies and shows on demand, anytime, anywhere. With
+              StreamVibe, you can enjoy a wide variety of content, including the
+              latest blockbusters, classic movies, popular TV shows, and more.
+              You can also create your own watchlists, so you can easily find
+              the content you want to watch.
+            </p>
+          </div>
+          <Button
+            className="hero__button"
+            iconName="play"
+            label="Start Watching Now"
+            hasFillIcon
+          />
+        </div>
       </div>
     </section>
   )
