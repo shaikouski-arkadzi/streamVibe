956252d0b63ea0fda753d7059c40b579432b19a5
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-21 22:51:20 +0300

    Add Categories to Home page

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -6,7 +6,6 @@ const Categories = () => {
     <Section
       title="Explore our wide variety of categories"
       titleId="categories-title"
-      aria-labelledby={titleId}
       description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
       actions={
         <div>
