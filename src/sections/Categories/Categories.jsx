f113ebe58d8a18f156a6e8a5a0286e45e1eb93d0
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-21 08:17:41 +0300

    Add Categories section base markup with Section wrapper

---
+++ src/sections/Categories/Categories.jsx
@@ -0,0 +1,22 @@
+import Section from "@/layouts/Section"
+import "./Categories.scss"
+
+const Categories = () => {
+  return (
+    <Section
+      title="Explore our wide variety of categories"
+      titleId="categories-title"
+      aria-labelledby={titleId}
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={
+        <div>
+          <button>Назад</button>
+          <button>Вперед</button>
+        </div>
+      }
+      isActionsHiddenOnMobile
+    ></Section>
+  )
+}
+
+export default Categories
