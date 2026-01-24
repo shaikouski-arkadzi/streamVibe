e3169ceb84a661710ae54823d57785cca1ec2023
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-24 11:45:50 +0300

    Add using SliderNavigation in Categories

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,6 +1,7 @@
 import Section from "@/layouts/Section"
 import CategoryCard from "@/components/CategoryCard"
 import Slider from "@/components/Slider"
+import SliderNavigation from "@/components/SliderNavigation"
 import "./Categories.scss"
 
 const categoryItems = [
@@ -52,20 +53,17 @@ const categoryItems = [
 ]
 
 const Categories = () => {
+  const sliderNavigationId = "categories-slider-navigation"
+
   return (
     <Section
       title="Explore our wide variety of categories"
       titleId="categories-title"
       description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
-      actions={
-        <div>
-          <button>Назад</button>
-          <button>Вперед</button>
-        </div>
-      }
+      actions={<SliderNavigation mode="tile" id={sliderNavigationId} />}
       isActionsHiddenOnMobile
     >
-      <Slider>
+      <Slider navigationTargetElementId={sliderNavigationId}>
         {categoryItems.map((categoryItem) => (
           <CategoryCard {...categoryItem} key={categoryItem.title} />
         ))}
