8f444f8d9df272b00b63f8ec929f5c7dc4d0941c
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-25 23:08:12 +0300

    Add function for getting data-js attribute value

---
+++ src/utils/getParams.js
@@ -0,0 +1,9 @@
+import getAttrNameFromSelector from "@/utils/getAttrNameFromSelector"
+
+const getParams = (element, dataAttrSelector) => {
+  return JSON.parse(
+    element.getAttribute(getAttrNameFromSelector(dataAttrSelector)),
+  )
+}
+
+export default getParams
