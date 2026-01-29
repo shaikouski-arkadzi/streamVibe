5df56205225e06290caf7ed013b167ff065df1e0
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-29 21:47:13 +0300

    Add Grid and DeviceCard in Devices component

--- src/sections/Devices/Devices.jsx
+++ src/sections/Devices/Devices.jsx
@@ -1,6 +1,46 @@
 import Section from "@/layouts/Section"
-
+import Grid from "@/components/Grid"
+import DeviceCard from "@/components/DeviceCard"
 import "./Devices.scss"
+
+const deviceItems = [
+  {
+    title: "Smartphones",
+    description:
+      "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+    imgSrc: "/devices/1.svg",
+  },
+  {
+    title: "Tablet",
+    description:
+      "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+    imgSrc: "/devices/2.svg",
+  },
+  {
+    title: "Smart TV",
+    description:
+      "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+    imgSrc: "/devices/3.svg",
+  },
+  {
+    title: "Laptops",
+    description:
+      "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+    imgSrc: "/devices/4.svg",
+  },
+  {
+    title: "Gaming Consoles",
+    description:
+      "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+    imgSrc: "/devices/5.svg",
+  },
+  {
+    title: "VR Headsets",
+    description:
+      "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store",
+    imgSrc: "/devices/6.svg",
+  },
+]
 
 const Devices = () => {
   return (
@@ -8,7 +48,13 @@ const Devices = () => {
       title="We Provide you streaming experience across various devices."
       titleId="devices-title"
       description="With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment."
-    ></Section>
+    >
+      <Grid columns={3}>
+        {deviceItems.map((deviceItem) => (
+          <DeviceCard {...deviceItem} key={deviceItem.title} />
+        ))}
+      </Grid>
+    </Section>
   )
 }
 
