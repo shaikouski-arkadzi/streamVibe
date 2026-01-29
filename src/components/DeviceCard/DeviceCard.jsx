6c78b6d545629960dafd6a379d43d3976da04339
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-29 21:46:06 +0300

    Add DeviceCard component

---
+++ src/components/DeviceCard/DeviceCard.jsx
@@ -0,0 +1,26 @@
+import "./DeviceCard.scss"
+
+const DeviceCard = ({ title, description, imgSrc }) => {
+  return (
+    <div className="device-card">
+      <header className="device-card__header">
+        <div className="device-card__image-wrapper">
+          <img
+            src={imgSrc}
+            alt=""
+            className="device-card__image"
+            width={40}
+            height={40}
+            loading="lazy"
+          />
+        </div>
+        <h3 className="device-card__title h4">{title}</h3>
+      </header>
+      <div className="device-card__description">
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default DeviceCard
