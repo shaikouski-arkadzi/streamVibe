f3d858e3e5a987497a5a50aaab88895ddb2d6809
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-30 22:57:21 +0300

    Add AccordionGroup to Section

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -1,6 +1,18 @@
 import Section from "@/layouts/Section"
 import Button from "@/components/Button"
+import AccordionGroup from "@/components/AccordionGroup"
 import "./Questions.scss"
+
+const questionItems = [
+  "What is StreamVibe?",
+  "How much does StreamVibe cost?",
+  "What content is available on StreamVibe?",
+  "How can I watch StreamVibe?",
+  "How do I sign up for StreamVibe?",
+  "What is the StreamVibe free trial?",
+  "How do I contact StreamVibe customer support?",
+  "What are the StreamVibe payment methods?",
+]
 
 const Questions = () => {
   return (
@@ -9,7 +21,9 @@ const Questions = () => {
       titleId="questions-title"
       description="Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe."
       actions={<Button label="Ask a Question" href="/support" />}
-    ></Section>
+    >
+      <AccordionGroup columns={2}>{questionItems}</AccordionGroup>
+    </Section>
   )
 }
 
