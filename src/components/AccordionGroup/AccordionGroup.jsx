63d4b81151e15ac0aafc43c9c317bab983d952f5
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-01-30 22:43:51 +0300

    Add AccordionGroup component

---
+++ src/components/AccordionGroup/AccordionGroup.jsx
@@ -0,0 +1,23 @@
+import cn from "classnames"
+import "./AccordionGroup.scss"
+
+const AccordionGroup = ({ columns = 1, children, isOrderedList = true }) => {
+  const ListTag = isOrderedList ? "ol" : "ul"
+
+  return (
+    <ListTag
+      className={cn("accordion-group", {
+        [`accordion-group--${columns}-columns`]: columns > 1,
+        "accordion-group-has-counter": isOrderedList,
+      })}
+    >
+      {children.map((child, index) => (
+        <li className="accordion-group__item" key={index}>
+          {child}
+        </li>
+      ))}
+    </ListTag>
+  )
+}
+
+export default AccordionGroup
