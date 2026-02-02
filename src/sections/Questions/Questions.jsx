3033a35f023c4fe9c9d15de327952929588b70a5
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-02 23:00:45 +0300

    Add Accordion to AccordionGroup

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -1,6 +1,7 @@
 import Section from "@/layouts/Section"
 import Button from "@/components/Button"
 import AccordionGroup from "@/components/AccordionGroup"
+import Accordion from "@/components/Accordion"
 import "./Questions.scss"
 
 const questionItems = [
@@ -22,7 +23,22 @@ const Questions = () => {
       description="Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe."
       actions={<Button label="Ask a Question" href="/support" />}
     >
-      <AccordionGroup columns={2}>{questionItems}</AccordionGroup>
+      <AccordionGroup columns={2}>
+        {questionItems.map((question, index) => (
+          <Accordion
+            title={question}
+            id={`question-&{index}`}
+            name="questions"
+            isOpen={index === 0}
+            key={question}
+          >
+            <p>
+              StreamVibe is a streaming service that allows you to watch movies
+              and shows on demand.
+            </p>
+          </Accordion>
+        ))}
+      </AccordionGroup>
     </Section>
   )
 }
