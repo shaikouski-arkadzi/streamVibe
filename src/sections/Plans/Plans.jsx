d1c88be3cd29c9c171389f0eda7448bd9d54d788
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-03 08:54:06 +0300

    Add Grid to Plans

--- src/sections/Plans/Plans.jsx
+++ src/sections/Plans/Plans.jsx
@@ -1,4 +1,5 @@
 import Section from "@/layouts/Section"
+import Grid from "@/components/Grid"
 import "./Plans.scss"
 
 const Plans = () => {
@@ -7,7 +8,9 @@ const Plans = () => {
       title="Choose the plan that's right for you"
       titleId="plans-title"
       description="Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!"
-    ></Section>
+    >
+      <Grid columns={3}></Grid>
+    </Section>
   )
 }
 
