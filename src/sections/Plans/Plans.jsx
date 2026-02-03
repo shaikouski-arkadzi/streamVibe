e038ea823840fd6c0d879321c2d1618930629677
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-03 08:40:46 +0300

    Add Plans section

---
+++ src/sections/Plans/Plans.jsx
@@ -0,0 +1,14 @@
+import Section from "@/layouts/Section"
+import "./Plans.scss"
+
+const Plans = () => {
+  return (
+    <Section
+      title="Choose the plan that's right for you"
+      titleId="plans-title"
+      description="Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!"
+    ></Section>
+  )
+}
+
+export default Plans
