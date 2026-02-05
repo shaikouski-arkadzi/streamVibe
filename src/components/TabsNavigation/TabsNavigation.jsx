fee799fc08838eeda8bc86f6e85c590785a87057
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-05 08:14:22 +0300

    Add TabsNavigation component

---
+++ src/components/TabsNavigation/TabsNavigation.jsx
@@ -0,0 +1,38 @@
+import cn from "classnames"
+import Button from "@/components/Button"
+import getIdFromTitle from "@/utils/getIdFromTitle"
+import "./TabsNavigation.scss"
+
+const TabsNavigation = ({ className, id, title, items = [] }) => {
+  const titleFormatted = getIdFromTitle(title)
+  const titleId = `${titleFormatted}-title`
+  return (
+    <div
+      className={cn(className, "tabs-navigation")}
+      id={id}
+      role="tablist"
+      aria-labelledby={titleId}
+      data-js-tabs-navigation=""
+    >
+      <h3 className="visually-hidden" id={titleId}>
+        {title}
+      </h3>
+      {items.map((item) => (
+        <div
+          className={cn("tabs-navigation__button", {
+            "is-active": item.isActive,
+          })}
+          role="tab"
+          aria-selected={item.isActive}
+          tabIndex={item.isActive ? 0 : -1}
+          data-js-tabs-button=""
+          key={item.title}
+        >
+          {item.title}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default TabsNavigation
