5673ffb10d02fafc3d330e20b5e8f22c244e5611
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-05 08:43:46 +0300

    Add TabsNavigation in actions in Plans component

--- src/sections/Plans/Plans.jsx
+++ src/sections/Plans/Plans.jsx
@@ -1,15 +1,26 @@
 import Section from "@/layouts/Section"
 import Grid from "@/components/Grid"
 import PlanCard from "@/components/PlanCard"
+import TabsNavigation from "@/components/TabsNavigation"
 import planGroups from "./planGroups"
 import "./Plans.scss"
 
 const Plans = () => {
+  const tabsTitle = "plans-tabs"
+  const tabsNavigationId = "plans-tabs-navigation"
+
   return (
     <Section
       title="Choose the plan that's right for you"
       titleId="plans-title"
       description="Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!"
+      actions={
+        <TabsNavigation
+          id={tabsNavigationId}
+          title={tabsTitle}
+          items={planGroups}
+        />
+      }
     >
       <Grid columns={3}>
         {planGroups[0].items.map((planItem) => (
