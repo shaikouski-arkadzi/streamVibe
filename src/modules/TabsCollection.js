8e0efcb85be6a4c987a15e3a9cc2e00b79766b35
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-06 08:45:43 +0300

    Add changing active tab in Tab

--- src/modules/TabsCollection.js
+++ src/modules/TabsCollection.js
@@ -31,6 +31,32 @@ class Tabs {
         ({ ariaSelected }) => ariaSelected,
       ),
     }
+    this.bindEvents()
+  }
+
+  updateUI() {
+    const { activeTabIndex } = this.state
+    this.buttonElements.forEach((buttonElement, index) => {
+      const isActive = index === activeTabIndex
+      buttonElement.classList.toggle(this.stateClasses.isActive, isActive)
+      buttonElement.ariaSelected = isActive
+      buttonElement.tabIndex = isActive ? 0 : -1
+    })
+    this.contentElements.forEach((contentElement, index) => {
+      const isActive = index === activeTabIndex
+      contentElement.classList.toggle(this.stateClasses.isActive, isActive)
+    })
+  }
+
+  onButtonClick(buttonIndex) {
+    this.state.activeTabIndex = buttonIndex
+    this.updateUI()
+  }
+
+  bindEvents() {
+    this.buttonElements.forEach((buttonElement, index) => {
+      buttonElement.addEventListener("click", () => this.onButtonClick(index))
+    })
   }
 }
 
