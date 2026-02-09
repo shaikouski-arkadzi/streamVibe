b357deebe6ec0ef8e3ba8a31a484911c3744f3d3
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-09 09:29:40 +0300

    Add proxy to BaseComponent

--- src/modules/generic/BaseComponent.js
+++ src/modules/generic/BaseComponent.js
@@ -1,4 +1,23 @@
 class BaseComponent {
+  getProxyState(initialState) {
+    return new Proxy(initialState, {
+      get: (target, prop) => {
+        return target[prop]
+      },
+      set: (target, prop, newValue) => {
+        const oldValue = target[prop]
+
+        target[prop] = newValue
+
+        if (newValue !== oldValue) {
+          this.updateUI
+        }
+
+        return
+      },
+    })
+  }
+
   /**
    * Перерисовка UI в ответ на обновление состояния
    */
