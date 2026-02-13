e1976a3c6c330b7e55b6ab1a36091b58670b0347
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-13 08:38:26 +0300

    Add Slider with MovieBannerCard to MovieBanner

--- src/sections/MoviesBanner/MoviesBanner.jsx
+++ src/sections/MoviesBanner/MoviesBanner.jsx
@@ -1,4 +1,33 @@
+import Slider from "@/components/Slider"
+import MovieBannerCard from "@/components/MovieBannerCard"
 import "./MoviesBanner.scss"
+
+const movieCards = [
+  {
+    title: "Avengers : Endgame",
+    description:
+      "With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.",
+    imgSrc: "/src/assets/images/movie-banner/1.jpg",
+  },
+  {
+    title: "Avengers : Endgame 2",
+    description:
+      "With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.",
+    imgSrc: "/src/assets/images/movie-banner/1.jpg",
+  },
+  {
+    title: "Avengers : Endgame 3",
+    description:
+      "With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.",
+    imgSrc: "/src/assets/images/movie-banner/1.jpg",
+  },
+  {
+    title: "Avengers : Endgame 4",
+    description:
+      "With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos's actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.",
+    imgSrc: "/src/assets/images/movie-banner/1.jpg",
+  },
+]
 
 const MoviesBanner = () => {
   const titleId = "movies-banner-title"
@@ -8,6 +37,15 @@ const MoviesBanner = () => {
       <h1 className="visually-hidden" id={titleId}>
         Movies & Shows
       </h1>
+      <Slider
+        sliderParams={{
+          slidesPerView: 1,
+        }}
+      >
+        {movieCards.map((movieCard) => (
+          <MovieBannerCard {...movieCard} key={movieCard.title} />
+        ))}
+      </Slider>
     </section>
   )
 }
