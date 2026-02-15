b1c82a1cb90a7aebee495fc0f17acf1ec24c430d
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-15 10:49:32 +0300

    Refactor: move categoryItems in Categories section in separate file

---
+++ src/sections/Categories/categoryItems.js
@@ -0,0 +1,184 @@
+const categoryItems = [
+  {
+    title: "Action 1",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Adventure 1",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Comedy 1",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Drama 1",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Horror 1",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Action 2",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Adventure 2",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Comedy 2",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Drama 2",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Horror 2",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Action 3",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Adventure 3",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Comedy 3",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Drama 3",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Horror 3",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Action 4",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Adventure 4",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Comedy 4",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Drama 4",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+  {
+    title: "Horror 4",
+    images: [
+      "/src/assets/images/categories/action/1.jpg",
+      "/src/assets/images/categories/action/2.jpg",
+      "/src/assets/images/categories/action/3.jpg",
+      "/src/assets/images/categories/action/4.jpg",
+    ],
+  },
+]
+
+export default categoryItems
