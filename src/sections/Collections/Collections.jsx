9a50cadb868d762553c97ff6afbac2552b9b1828
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-15 10:23:15 +0300

    Add starter Collections component

---
+++ src/sections/Collections/Collections.jsx
@@ -0,0 +1,8 @@
+import Tabs from "@/components/Tabs"
+import "./Collections.scss"
+
+const Collections = () => {
+  return <Tabs className="collections container" title="collections" />
+}
+
+export default Collections
