7d35d56b6ac88f87d8c6fdc8c8ba1575e4a0dce0
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-15 11:18:05 +0300

    Add data in Tabs in Collections

--- src/sections/Collections/Collections.jsx
+++ src/sections/Collections/Collections.jsx
@@ -1,8 +1,64 @@
 import Tabs from "@/components/Tabs"
+import SliderNavigation from "@/components/SliderNavigation"
+import Slider from "@/components/Slider"
+import CategoryCard from "@/components/CategoryCard"
+import Section from "@/layouts/Section"
+import getIdFromTitle from "@/utils/getIdFromTitle"
+import collectionGroups from "./collectionGroups"
 import "./Collections.scss"
 
 const Collections = () => {
-  return <Tabs className="collections container" title="collections" />
+  return (
+    <Tabs
+      className="collections container"
+      title="collections"
+      isEnableOnlyOnMobile
+      items={collectionGroups.map((collectionGroup) => ({
+        isActive: collectionGroup.isActive,
+        title: collectionGroup.title,
+        children: (
+          <div className="collections__group">
+            <p className="collections__title hidden-mobile">
+              {collectionGroup.title}
+            </p>
+            {collectionGroup.items.map(
+              ({ title, categoryItems, sliderParams }) => {
+                const titleFormatted = `${getIdFromTitle(collectionGroup.title)}-${getIdFromTitle(title)}`
+                const titleId = `${titleFormatted}-title`
+                const sliderNavigationId = `${titleFormatted}-slider-navigation`
+
+                return (
+                  <Section
+                    className="collections__section"
+                    title={title}
+                    titleId={titleId}
+                    actions={
+                      <SliderNavigation id={sliderNavigationId} mode="tile" />
+                    }
+                    isActionsHiddenOnMobile
+                    key={titleId}
+                  >
+                    <Slider
+                      sliderParams={sliderParams}
+                      navigationTargetElementId={sliderNavigationId}
+                      isBeyondTheViewportOnMobileS
+                    >
+                      {categoryItems.map((categoryItem) => (
+                        <CategoryCard
+                          {...categoryItem}
+                          key={categoryItem.title}
+                        />
+                      ))}
+                    </Slider>
+                  </Section>
+                )
+              },
+            )}
+          </div>
+        ),
+      }))}
+    />
+  )
 }
 
 export default Collections
