2e8d20013f5bde6897ad9374d567446c2329e0a8
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-17 09:20:49 +0300

    Add aria-label to Icon

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,9 +2,9 @@ import cn from "classnames"
 import { Icon as MinistaIcon } from "minista"
 import "./Icon.scss"
 
-const Icon = ({ className, name, hasFill = false }) => {
+const Icon = ({ className, name, hasFill = false, ariaLabel }) => {
   return (
-    <span className={cn(className, "icon")}>
+    <span className={cn(className, "icon")} aria-label={ariaLabel}>
       <MinistaIcon
         iconId={name}
         fill={hasFill ? "currentColor" : "none"}
