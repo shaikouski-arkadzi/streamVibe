328adea37353001d6b1a89b15f022cb56e4f2a64
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-17 09:17:14 +0300

    Add MovieCard component

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,17 @@
+import { Image } from "minista"
+import Badge from "@/components/Badge"
+import "./MovieCard.scss"
+
+const MovieCard = ({ title, imgSrc, duration, views, href = "/movie" }) => {
+  return (
+    <a className="movie-card" href={href} title={title}>
+      <h3 className="visually-hidden">{title}</h3>
+      <Image src={imgSrc} className="movie-card__image" />
+      <div className="movie-card__body">
+        {duration && <Badge>{duration}</Badge>}
+      </div>
+    </a>
+  )
+}
+
+export default MovieCard
