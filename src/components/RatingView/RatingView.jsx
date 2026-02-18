6800417dc8e6b6e3d74f329eb7197dce4a5aa5ca
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-18 08:52:51 +0300

    Add RatingView component

---
+++ src/components/RatingView/RatingView.jsx
@@ -0,0 +1,36 @@
+import cn from "classnames"
+
+import "./RatingView.scss"
+
+const RatingView = ({ label, value = 5 }) => {
+  const ariaLabel = `Rating: ${value} stars`
+
+  return (
+    <div
+      className="rating-view"
+      aria-label={ariaLabel}
+      title={ariaLabel}
+      style={{ "--ratingViewValue": value }}
+    >
+      <div className="rating-view__stars">
+        <img
+          className="rating-view__stars-unfilled"
+          src="/rating/stars_unfilled.svg"
+          width={98}
+          height={18}
+          alt=""
+        />
+        <img
+          className="rating-view__stars-filled"
+          src="/rating/stars_filled.svg"
+          width={98}
+          height={18}
+          alt=""
+        />
+      </div>
+      {label && <div className="rating-view__label">{label}</div>}
+    </div>
+  )
+}
+
+export default RatingView
