0f7fa4ecd1498dcb515b18c72b844929edeccf62
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-20 09:19:56 +0300

    Add MovieBanner to Movie page

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,7 +1,13 @@
+import MovieBanner from "@/sections/MovieBanner"
+
 export const metadata = {
   title: "Movie",
 }
 
 export default function () {
-  return <></>
+  return (
+    <>
+      <MovieBanner />
+    </>
+  )
 }
