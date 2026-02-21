dde7ebf255b382efd8b1182e9386539e9ad34d68
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-21 08:20:02 +0300

    Add using isSmallPaddingY for MovieBannerCard in MovieBanner component

--- src/sections/MovieBanner/MovieBanner.jsx
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -12,6 +12,7 @@ const MovieBanner = () => {
         TitleHTag="h1"
         description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
         imgSrc="/src/assets/images/movie-banner/2.jpg"
+        isSmallPaddingY
       />
     </section>
   )
