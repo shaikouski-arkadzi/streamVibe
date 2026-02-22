46f2a8f034e667c3e112d41685618d21fe3fc51d
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-22 09:33:21 +0300

    Add starter MovieDetails section

---
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,44 @@
+import SliderNavigation from "@/components/SliderNavigation"
+import "./MovieDetails.scss"
+
+const MovieDetails = () => {
+  const titleId = "movie-details-title"
+  const castSliderNavigationId = "movie-cast-slider-navigation"
+
+  return (
+    <section className="movie-details container" aria-labelledby={titleId}>
+      <h2 className="visually-hidden" id={titleId}>
+        Detailed movie information
+      </h2>
+      <div className="movie-details__main">
+        <div className="movie-details__panel">
+          <div className="movie-details__group">
+            <h3 className="movie-details__title">Description</h3>
+            <div className="movie-details__description">
+              <p>
+                A fiery young man clashes with an unflinching forest officer in
+                a south Indian village where spirituality, fate and folklore
+                rule the lands.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="movie-details__panel">
+          <header className="movie-details__panel-header">
+            <h3 className="movie-details__title">Cast</h3>
+            <SliderNavigation
+              id={castSliderNavigationId}
+              hasPagination={false}
+            />
+          </header>
+        </div>
+        <div className="movie-details__panel"></div>
+      </div>
+      <aside className="movie-details__info">
+        <div className="movie-details__panel"></div>
+      </aside>
+    </section>
+  )
+}
+
+export default MovieDetails
