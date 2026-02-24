969db1ccfda042f0b701ee2fa5cded2b4f5e2c2d
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-24 08:00:48 +0300

    Add Tags component

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,17 @@
+import "./Badge.scss"
+
+const Tags = ({ items = [] }) => {
+  return (
+    <div className="tags">
+      <ul className="tags__list">
+        {items.map((tag) => (
+          <li className="tags__item" key={tag}>
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Tags
