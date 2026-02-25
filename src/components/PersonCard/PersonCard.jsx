180b3a0270edd81a37f95a09728414d3527c66e7
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-25 09:30:29 +0300

    Add name and subtitle to PersonCard component

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -1,7 +1,9 @@
 import { Image } from "minista"
 import "./PersonCard.scss"
 
-const PersonCard = ({ imgSrc, imgAlt }) => {
+const PersonCard = ({ imgSrc, imgAlt, name, subtitle }) => {
+  const hasBody = Boolean(name || subtitle)
+
   return (
     <div className="person-card">
       <Image
@@ -10,6 +12,12 @@ const PersonCard = ({ imgSrc, imgAlt }) => {
         alt={imgAlt}
         title={imgAlt}
       />
+      {hasBody && (
+        <div className="person-card__body">
+          {name && <h4 className="person-card__name">{name}</h4>}
+          {subtitle && <p className="person-card__subtitle">{subtitle}</p>}
+        </div>
+      )}
     </div>
   )
 }
