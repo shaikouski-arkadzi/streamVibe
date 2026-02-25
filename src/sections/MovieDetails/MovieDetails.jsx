10f2db6aa6a655add04399fb215107514c57a583
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-25 10:11:33 +0300

    Add changing order of section on mobile in MovieDetails

--- src/sections/MovieDetails/MovieDetails.jsx
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -120,7 +120,7 @@ const MovieDetails = () => {
         Detailed movie information
       </h2>
       <div className="movie-details__main">
-        <div className="movie-details__panel">
+        <div className="movie-details__panel movie-details__panel--description">
           <div className="movie-details__group">
             <h3 className="movie-details__title">Description</h3>
             <div className="movie-details__description">
