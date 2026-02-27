a0eacb9f729d696ae9c6ce8709d33cf1289f79c4
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-27 08:55:57 +0300

    Add EpisodeCard component

---
+++ src/components/EpisodeCard/EpisodeCard.jsx
@@ -0,0 +1,42 @@
+import Icon from "@/components/Icon"
+import "./EpisodeCard.scss"
+
+const EpisodeCard = ({ number, title, description, duration, video }) => {
+  const playButtonTitle = "Play video"
+
+  return (
+    <div className="episode-card">
+      <div className="episode-card__number">{number}</div>
+      <div className="episode-card__player">
+        <video
+          src={video.src}
+          poster={video.poster}
+          className="episode-card__video"
+          width={172}
+          height={118}
+        />
+        <button
+          className="episode-card__play-button is-active"
+          aria-label={playButtonTitle}
+          title={playButtonTitle}
+        >
+          <Icon className="episode-card__play-button-icon" name="play-circle" />
+        </button>
+      </div>
+      <div className="episode-card__body">
+        <div className="episode-card__indo">
+          <h4 className="episode-card__title h6">{title}</h4>
+          <div className="episode-card__duration">
+            <Icon name="clock_stroke" ariaLabel="Duration" />
+            <span>{duration}</span>
+          </div>
+        </div>
+        <div className="episode-card__description hidden-mobile">
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EpisodeCard
