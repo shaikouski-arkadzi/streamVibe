97d833c28c8769138ab1d2ceef5a53216cfa543a
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-27 08:08:19 +0300

    Add dark mode to AccordionGroup component

--- src/components/AccordionGroup/AccordionGroup.jsx
+++ src/components/AccordionGroup/AccordionGroup.jsx
@@ -1,7 +1,15 @@
 import cn from "classnames"
 import "./AccordionGroup.scss"
 
-const AccordionGroup = ({ columns = 1, children, isOrderedList = true }) => {
+const AccordionGroup = ({
+  /**
+   * '' (default) | 'dark'
+   */
+  mode = "",
+  columns = 1,
+  children,
+  isOrderedList = true,
+}) => {
   const itemsPerColumn = Math.ceil(children.length / columns)
   const ListTag = isOrderedList ? "ol" : "ul"
 
@@ -10,6 +18,7 @@ const AccordionGroup = ({ columns = 1, children, isOrderedList = true }) => {
       className={cn("accordion-group", {
         [`accordion-group--${columns}-columns`]: columns > 1,
         "accordion-group--has-counter": isOrderedList,
+        [`accordion-group--${mode}`]: mode,
       })}
     >
       {children.map((child, index) => (
