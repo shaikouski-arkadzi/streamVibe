f45dd7d2cc82d794c4b9cf535ee40e10769e5206
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-27 08:29:16 +0300

    Add AccordionGroup and Accordion components to Seasons component

--- src/components/Seasons/Seasons.jsx
+++ src/components/Seasons/Seasons.jsx
@@ -1,7 +1,28 @@
+import Accordion from "../Accordion"
+import AccordionGroup from "../AccordionGroup"
+import seasonItems from "./seasonItems"
 import "./Seasons.scss"
 
 const Seasons = () => {
-  return <>Seasons</>
+  return (
+    <AccordionGroup mode="dark" isOrderedList={false}>
+      {seasonItems.map(({ title, subtitle, episodes }, index) => (
+        <Accordion
+          title={title}
+          titleLevelClassName="h4"
+          subtitle={subtitle}
+          id={`season-${title}-${subtitle}`}
+          name="seasons"
+          isOpen={index === 0}
+          key={`season-${title}-${subtitle}`}
+          isArrowButton
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium,
+          quisquam.
+        </Accordion>
+      ))}
+    </AccordionGroup>
+  )
 }
 
 export default Seasons
