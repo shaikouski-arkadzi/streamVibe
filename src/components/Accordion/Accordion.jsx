0a92f6d5343d8becdeb072b9907b4428cfc9c4c3
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-02-27 08:27:46 +0300

    Add new view for Accordion component needed in Seasons component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,14 +1,33 @@
+import cn from "classnames"
+import Icon from "@/components/Icon"
 import "./Accordion.scss"
 
-const Accordion = ({ title, id, name, isOpen, children }) => {
+const Accordion = ({
+  title,
+  titleLevelClassName = "h5",
+  subtitle,
+  id,
+  name,
+  isOpen,
+  children,
+  isArrowButton = false,
+}) => {
   return (
     <div className="accordion">
       <details className="accordion__details" name={name} open={isOpen}>
         <summary className="accordion__summary">
-          <h3 className="accordion__title h5">
+          <h3 className={cn("accordion__title", titleLevelClassName)}>
             <span role="term" aria-details={id}>
               {title}
             </span>
+            {subtitle && (
+              <span className="accordion__subtitle">{subtitle}</span>
+            )}
+            {isArrowButton && (
+              <div className="accordion__arrow">
+                <Icon name="arrow-down" />
+              </div>
+            )}
           </h3>
         </summary>
       </details>
