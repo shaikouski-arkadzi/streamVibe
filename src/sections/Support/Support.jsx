9fba17b3f4c7b3751605a96c7c7ff3c7146d7025
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-01 08:23:23 +0300

    Add starter Support section

---
+++ src/sections/Support/Support.jsx
@@ -0,0 +1,31 @@
+import { Image } from "minista"
+import "./Support.scss"
+
+const Support = () => {
+  const titleId = "support-title"
+
+  return (
+    <section className="support container" aria-labelledby={titleId}>
+      <div className="support__body">
+        <div className="support__info">
+          <h1 className="support__title h2" id={titleId}>
+            Welcome to our support page!
+          </h1>
+          <div className="support__description">
+            <p>
+              We're here to help you with any problems you may be having with
+              our product.
+            </p>
+          </div>
+        </div>
+        <Image
+          className="support__image"
+          src="/src/assets/images/support/1.png"
+        />
+      </div>
+      <form className="support__form" action=""></form>
+    </section>
+  )
+}
+
+export default Support
