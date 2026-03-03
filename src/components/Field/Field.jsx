faf1b63cc7b8b0f1fdbba911c6f6445b358b2a0d
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-03 09:28:54 +0300

    Add Field component

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,43 @@
+import cn from "classnames"
+import getIdFromTitle from "@/utils/getIdFromTitle"
+import "./Field.scss"
+
+const Field = ({
+  className,
+  id,
+  label,
+  /**
+   * udefined(default) | 'email' | 'textarea'
+   */
+  type,
+  placeholder,
+  isRequired,
+}) => {
+  const fieldId = id ?? getIdFromTitle(label)
+
+  const Component = type === "textarea" ? "textarea" : "input"
+
+  return (
+    <div className={cn(className, "field")}>
+      <label className="field__label" htmlFor={fieldId}>
+        {label}{" "}
+        {isRequired && (
+          <span className="field__required-star" aria-hidden={true}>
+            *
+          </span>
+        )}
+      </label>
+      <div className="field__body">
+        <Component
+          className="field__control"
+          id={fieldId}
+          type={type}
+          placeholder={placeholder}
+          required={isRequired}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Field
