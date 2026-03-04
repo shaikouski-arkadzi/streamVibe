bc5460ee26d88d7f541e2dc69a412ba645750372
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-04 08:33:37 +0300

    Add Field components to Support section

--- src/sections/Support/Support.jsx
+++ src/sections/Support/Support.jsx
@@ -1,4 +1,5 @@
 import { Image } from "minista"
+import Field from "@/components/Field"
 import "./Support.scss"
 
 const Support = () => {
@@ -23,7 +24,39 @@ const Support = () => {
           src="/src/assets/images/support/1.png"
         />
       </div>
-      <form className="support__form" action=""></form>
+      <form className="support__form" action="">
+        <Field
+          className="support__form-cell"
+          label="First Name"
+          placeholder="Ivan"
+          isRequired
+        />
+        <Field
+          className="support__form-cell"
+          label="Last Name"
+          placeholder="Ivanov"
+        />
+        <Field
+          className="support__form-cell"
+          label="Email"
+          type="email"
+          placeholder="example@example.com"
+          isRequired
+        />
+        <Field
+          className="support__form-cell"
+          label="Phone Number"
+          placeholder="(999) 999-99-99"
+          inputMode="tel"
+        />
+        <Field
+          className="support__form-cell support__form-cell--wide"
+          label="Message"
+          type="textarea"
+          placeholder="Hi! I have a question..."
+          isRequired
+        />
+      </form>
     </section>
   )
 }
