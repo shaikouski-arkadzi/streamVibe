63545813d7f42850f878cf2e0a984198216d5630
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-06 08:25:01 +0300

    Add Checkbox to Support section and styles for checkbox wrapper

--- src/sections/Support/Support.jsx
+++ src/sections/Support/Support.jsx
@@ -1,5 +1,7 @@
 import { Image } from "minista"
 import Field from "@/components/Field"
+import Checkbox from "@/components/Checkbox"
+import Button from "@/components/Button"
 import "./Support.scss"
 
 const Support = () => {
@@ -57,6 +59,18 @@ const Support = () => {
           placeholder="Hi! I have a question..."
           isRequired
         />
+        <div className="support__form-cell support__form-cell--wide support__form-cell--actions">
+          <Checkbox
+            className="support__form-agreement"
+            label="I agree with Terms of Use and Privacy Policy"
+            isRequired
+          />
+          <Button
+            className="support__form-submit-button"
+            label="Send Message"
+            type="submit"
+          />
+        </div>
       </form>
     </section>
   )
