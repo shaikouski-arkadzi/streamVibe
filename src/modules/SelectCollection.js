ec625e3df7d9e714d480970df4d1495d187eee93
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-10 10:14:54 +0300

    Add initial SelectCollection script with updateUI method

---
+++ src/modules/SelectCollection.js
@@ -0,0 +1,113 @@
+const { default: BaseComponent } = require("./generic/BaseComponent")
+
+const rootSelector = "[data-js-select]"
+
+class Select extends BaseComponent {
+  selectors = {
+    root: rootSelector,
+    originalControl: "[data-js-select-original-control]",
+    button: "[data-js-select-button]",
+    dropdown: "[data-js-select-dropdown]",
+    option: "[data-js-select-option]",
+  }
+
+  stateClasses = {
+    isExpanded: "is-expanded",
+    isSelected: "is-selected",
+    isCurrent: "is-current",
+    isOnTheLeftSide: "is-on-the-left-side",
+    isOnTheRightSide: "is-on-the-right-side",
+  }
+
+  initialState = {
+    isExpanded: false,
+    currentOptionIndex: null,
+    selectedOptionElement: null,
+  }
+
+  constructor(rootElement) {
+    super()
+    this.rootElement = rootElement
+    this.originalControlElement = this.rootElement.querySelector(
+      this.selectors.originalControl,
+    )
+    this.buttonElement = this.rootElement.querySelector(this.selectors.button)
+    this.dropdownElement = this.rootElement.querySelector(
+      this.selectors.dropdown,
+    )
+    this.optionElements = this.dropdownElement.querySelectorAll(
+      this.selectors.option,
+    )
+    this.state = this.getProxyState({
+      ...this.initialState,
+      // Берётся индекс выбранного option из <select>
+      currentOptionIndex: this.originalControlElement.selectedIndex,
+      // Соответствующий DOM option
+      selectedOptionElement:
+        this.optionElements[this.originalControlElement.selectedIndex],
+    })
+  }
+
+  updateUI() {
+    const { isExpanded, currentOptionIndex, selectedOptionElement } = this.state
+
+    // Получаем текст выбранного option
+    const newSelectedOptionValue = selectedOptionElement.textContent.trim()
+
+    // Синхронизируем кастомный select с настоящим <select>
+    const updateOriginalControl = () => {
+      this.originalControlElement.value = newSelectedOptionValue
+    }
+
+    // Текст кнопки = выбранный option
+    const updateButton = () => {
+      this.buttonElement.textContent = newSelectedOptionValue
+      this.buttonElement.classList.toggle(
+        this.stateClasses.isExpanded,
+        isExpanded,
+      )
+      this.buttonElement.ariaExpanded = isExpanded
+      this.buttonElement.ariaActiveDescendant =
+        this.optionElements[currentOptionIndex].id
+    }
+
+    const updateDropdown = () => {
+      this.dropdownElement.classList.toggle(
+        this.stateClasses.isExpanded,
+        isExpanded,
+      )
+    }
+
+    const updateOptions = () => {
+      this.optionElements.forEach((optionElement, index) => {
+        // Это option под курсором / клавиатурой
+        const isCurrent = currentOptionIndex === index
+        // Это реально выбранный option
+        const isSelected = selectedOptionElement === optionElement
+
+        optionElement.classList.toggle(this.stateClasses.isCurrent, isCurrent)
+        optionElement.classList.toggle(this.stateClasses.isSelected, isSelected)
+        optionElement.ariaSelected = isSelected
+      })
+    }
+
+    updateOriginalControl()
+    updateButton()
+    updateDropdown()
+    updateOptions()
+  }
+}
+
+class SelectCollection {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    document
+      .querySelectorAll(rootSelector)
+      .forEach((element) => new Select(element))
+  }
+}
+
+export default SelectCollection
