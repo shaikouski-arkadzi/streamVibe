a1d8ab2f989cb661df8fc18e268d699cb01baebd
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-14 08:21:05 +0300

    Add starter PlansComparison section

---
+++ src/sections/PlansComparison/PlansComparison.jsx
@@ -0,0 +1,14 @@
+import Section from "@/layouts/Section"
+import "./PlansComparison.scss"
+
+const PlansComparison = () => {
+  return (
+    <Section
+      title="Compare our plans and find the right one for you"
+      titleId="plans-comparison-title"
+      description="StreamVibe offers three different plans to fit your needs: Basic, Standard, and Premium. Compare the features of each plan and choose the one that's right for you."
+    ></Section>
+  )
+}
+
+export default PlansComparison
