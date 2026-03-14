7d36fd355d303a5186aed69537fb1e3b522745e0
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-14 08:14:18 +0300

    Add subscriptions page

---
+++ src/pages/subscriptions.jsx
@@ -0,0 +1,13 @@
+import Plans from "@/sections/Plans"
+
+export const metadata = {
+  title: "Subscriptions",
+}
+
+export default function () {
+  return (
+    <>
+      <Plans />
+    </>
+  )
+}
