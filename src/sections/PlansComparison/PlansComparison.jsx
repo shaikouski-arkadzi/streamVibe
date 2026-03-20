eef1ed1d030d519dc8c9a2448896acd3225654fc
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-20 19:31:13 +0300

    Add Specifications with Tabs on mobile to PlansComparison

--- src/sections/PlansComparison/PlansComparison.jsx
+++ src/sections/PlansComparison/PlansComparison.jsx
@@ -1,6 +1,8 @@
 import Section from "@/layouts/Section"
 import Badge from "@/components/Badge"
 import Table from "@/components/Table"
+import Specifications from "@/components/Specifications"
+import Tabs from "@/components/Tabs"
 import { rows } from "./planTableRows"
 import "./PlansComparison.scss"
 
@@ -31,13 +33,34 @@ const PlansComparison = () => {
     },
   ]
 
+  const tabsItems = headCells
+    .filter((headCell) => headCell.tabsTitle)
+    .map((headCell, headCellIndex) => ({
+      title: headCell.tabsTitle,
+      isActive: headCellIndex === 0,
+      children: (
+        <Specifications
+          items={rows.map(({ cells, isWide }) => ({
+            key: cells[0],
+            value: cells[headCellIndex + 1],
+            isWide,
+          }))}
+        />
+      ),
+    }))
+
   return (
     <Section
       title="Compare our plans and find the right one for you"
       titleId="plans-comparison-title"
       description="StreamVibe offers three different plans to fit your needs: Basic, Standard, and Premium. Compare the features of each plan and choose the one that's right for you."
     >
-      <Table headCells={headCells} rows={rows} />
+      <Table className="hidden-mobile" headCells={headCells} rows={rows} />
+      <Tabs
+        className="visible-mobile"
+        title="plans-comparison-tabs-title"
+        items={tabsItems}
+      />
     </Section>
   )
 }
